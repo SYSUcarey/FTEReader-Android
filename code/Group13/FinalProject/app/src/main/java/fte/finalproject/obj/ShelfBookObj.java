943175784120f5ddca3ca5fd96567506d549c478
Carter Che <128522426@qq.com>
2018-12-30 22:13:44 +0800

    基本确定数据库结构，完善数据库操作

--- code/Group13/FinalProject/app/src/main/java/fte/finalproject/obj/ShelfBookObj.java
+++ code/Group13/FinalProject/app/src/main/java/fte/finalproject/obj/ShelfBookObj.java
@@ -3,14 +3,20 @@ package fte.finalproject.obj;
 import android.graphics.Bitmap;
 
 public class ShelfBookObj {
+    int bookId;
     private Bitmap icon;
     private String name;
-    private int readPage;
+    int type;
+    private String address;
+    private int readChapter;
 
-    public ShelfBookObj(String name, Bitmap icon) {
+    public ShelfBookObj(int bookId, String name, Bitmap icon, int readChapter, String address,int type) {
+        this.bookId = bookId;
         this.name = name;
         this.icon = icon;
-        readPage = 0;
+        this.readChapter = readChapter;
+        this.address = address;
+        this.type = type;
     }
 
     public String getName() {
@@ -21,7 +27,43 @@ public class ShelfBookObj {
         return icon;
     }
 
-    public int getReadPage() {
-        return readPage;
+    public String getAddress() {
+        return address;
+    }
+
+    public void setIcon(Bitmap icon) {
+        this.icon = icon;
+    }
+
+    public int getReadChapter() {
+        return readChapter;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setReadChapter(int readChapter) {
+        this.readChapter = readChapter;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
     }
 }
